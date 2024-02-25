280eb70bdcddf18d6925d75759524538f3e031fa
randradee <dev.renatopandrade@gmail.com>
2024-02-25 19:28:48 -0300

    setup prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThingsModule } from './things/things.module';
+import { PrismaService } from './common/prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ThingsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
